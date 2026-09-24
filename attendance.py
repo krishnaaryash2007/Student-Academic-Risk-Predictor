7d1d2b716b2c77968a8da60855c85eda032f37ce
Krishna Aryash <krishnaaryash2007@gmail.com>
2026-09-24 12:30:10 +0530

    updated attendance.py

--- attendance.py
+++ attendance.py
@@ -1,7 +1,10 @@
 def attendance_status(attendance):
 
-    if attendance >= 75:
+    if attendance==100:
+        return " Excellent Attendance"
+    elif attendance >= 90 and attendance < 100:
+        return " Very Good Attendance"
+    elif attendance >= 75 and attendance < 90:
         return "Good Attendance"
-
     else:
-        return "Less Attendance"
+        return "Low Attendance"
